4d78d87aafe0dedaaaf34795705ec2f453e082c7
Samyak Gandhi <Samyaksg01@gmail.com>
2024-08-30 11:22:13 +0530

    project pushed

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+const baseURL =
+  "https://api.weatherapi.com/v1/current.json?key=79dbf0fbe49f4170a00135412242708";
+
+export const getWeatherDataForCity = async (city) => {
+  const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
+  return await response.json();
+};
+
+export const getWeatherDataForLocation = async (lat, lon) => {
+  const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
+  return await response.json();
+};
